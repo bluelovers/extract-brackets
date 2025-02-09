ccdde56c621a24f2d714489435c47a6c7ed1afc4
bluelovers <codelovers@users.sourceforge.net>
2025-02-10 05:53:25 +0800

    refactor: `Extraction` to class

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,10 @@ Add endIndex
 
 import { buildRegex, buildStringObj, getMate, popLast } from './utils';
 
-//To extract everything inside the outer most brackets
-class Extractor
+/**
+ * To extract everything inside the outer most brackets
+ */
+export class Extractor
 {
 	protected regex: RegExp;
 	protected openChar: string;
@@ -45,166 +47,178 @@ class Extractor
 	}
 }
 
-function Extraction(open, close, stringChars, regex)
+export class Extraction
 {
-	this.matches = [];
-	this.tree = [];
-	this.escaped = false;
+	protected matches: any[];
+	protected tree: any[];
+	protected escaped: boolean;
+	protected openChar: string;
+	protected closeChar: string;
+	protected stringChars: any;
+	protected regex: RegExp;
+	protected sameChar: boolean;
+	protected count: number;
+	protected index: number;
+	protected result: any;
+	protected unescapeStr: string;
 
-	this.openChar = open;
-	this.closeChar = close;
-	this.stringChars = stringChars;
-	this.regex = regex;
-
-	this.sameChar = open === close;
-}
-
-Extraction.prototype.init = function (str, count)
-{
-	var arr = str.split(this.regex),
-		l = arr.length,
-		i;
-	this.count = count;
-	this.index = 0;
-
-	for (i = 0; i < l; i++)
+	constructor(open: string, close: string, stringChars: any, regex: RegExp)
 	{
-		if (this.handleStr(arr[i])) break;
-		this.index += arr[i].length;
+		this.matches = [];
+		this.tree = [];
+		this.escaped = false;
+
+		this.openChar = open;
+		this.closeChar = close;
+		this.stringChars = stringChars;
+		this.regex = regex;
+
+		this.sameChar = open === close;
 	}
 
-	//if there is still a result, then there was an error
-	if (this.result)
+	init(str: string, count: number): any[]
 	{
-		console.dir(this)
-		if (this.escaped) throw new Error("Unable to parse. Unclosed String detected")
-		throw new Error("Unable to parse. Unclosed Bracket detected")
+		const arr = str.split(this.regex);
+		const l = arr.length;
+		this.count = count;
+		this.index = 0;
+
+		for (let i = 0; i < l; i++)
+		{
+			if (this.handleStr(arr[i])) break;
+			this.index += arr[i].length;
+		}
+
+		//if there is still a result, then there was an error
+		if (this.result)
+		{
+			console.dir(this);
+			if (this.escaped) throw new Error("Unable to parse. Unclosed String detected");
+			throw new Error("Unable to parse. Unclosed Bracket detected");
+		}
+
+		return this.matches;
 	}
 
-	return this.matches;
-}
-
-Extraction.prototype.handleStr = function (str)
-{
-	var index;
-
-	if (this.escaped)
+	handleStr(str: string): boolean
 	{
-		if (str === this.unescapeStr) this.escaped = false;
+		let index: number;
+
+		if (this.escaped)
+		{
+			if (str === this.unescapeStr) this.escaped = false;
+			return this.add(str);
+		}
+
+		if (str === this.openChar) return this.open();
+
+		if (str === this.closeChar) return this.close();
+
+		index = this.stringChars.open.indexOf(str);
+		if (index > -1)
+		{
+			this.escaped = true;
+			this.unescapeStr = this.stringChars.close[index];
+		}
 		return this.add(str);
 	}
 
-	if (str === this.openChar) return this.open();
-
-	if (str === this.closeChar) return this.close();
-
-	index = this.stringChars.open.indexOf(str);
-	if (index > -1)
+	open(): boolean
 	{
-		this.escaped = true;
-		this.unescapeStr = this.stringChars.close[index];
-	}
-	return this.add(str);
-}
+		//create a new result object
+		const obj: any = {
+			nest: [],
+			simple: [],
+			hasNest: false,
+			str: '',
+			index: [this.index],
+		};
 
-Extraction.prototype.open = function ()
-{
-	//create a new result object
-	var self = this;
-	obj = {
-		nest: [],
-		simple: [],
-		hasNest: false,
-		str: '',
-		index: [this.index]
-	};
-
-	//if there currently is a result:
-	if (this.result)
-	{
-
-		//if the open and close characters are the same, then close
-		if (this.sameChar) return this.close();
-
-		//set hasNest to true
-		this.result.hasNest = true;
-
-		//add the new result object to the current nest
-		this.result.nest.push(obj);
-
-		//add the new simple result object to the current simple
-		this.result.simple.push(obj.simple);
-
-		//add the result to the tree
-		this.tree.push(this.result);
-
-		this.tree.forEach(function (branch, i)
+		//if there currently is a result:
+		if (this.result)
 		{
-			//get the index of this for each parent
-			obj.index.push(self.index - branch.index[0] - 1)
+			//if the open and close characters are the same, then close
+			if (this.sameChar) return this.close();
 
-			//add the open char to all strings
-			branch.str += self.openChar;
-		});
-	}
-	//otherwise, save the obj as a new match
-	else this.matches.push(obj);
+			//set hasNest to true
+			this.result.hasNest = true;
 
-	//set the result to be the new object
-	this.result = obj;
+			//add the new result object to the current nest
+			this.result.nest.push(obj);
 
-	return false;
-}
+			//add the new simple result object to the current simple
+			this.result.simple.push(obj.simple);
 
-//to add text to the current result if there is one
-Extraction.prototype.add = function (str)
-{
-	var nest;
+			//add the result to the tree
+			this.tree.push(this.result);
 
-	if (str && this.result)
-	{
-		nest = this.result.nest;
+			this.tree.forEach((branch, i) =>
+			{
+				//get the index of this for each parent
+				obj.index.push(this.index - branch.index[0] - 1);
 
-		//if the last element is a string, then append
-		if (typeof nest[nest.length - 1] === "string")
-		{
-			nest[nest.length - 1] += str;
-			this.result.simple += str;
+				//add the open char to all strings
+				branch.str += this.openChar;
+			});
 		}
-		//otherwise, the new string
+		//otherwise, save the obj as a new match
 		else
 		{
-			nest.push(str);
-			this.result.simple.push(str);
+			this.matches.push(obj);
 		}
 
-		this.result.str += str;
+		//set the result to be the new object
+		this.result = obj;
 
-		this.tree.forEach(function (obj)
-		{
-			obj.str += str;
-		})
+		return false;
 	}
 
-	return false;
-}
-
-Extraction.prototype.close = function ()
-{
-
-	var self = this;
-
-	this.tree.forEach(function (branch, i)
+	add(str: string): boolean
 	{
-		//add the close char to all strings
-		branch.str += self.closeChar;
-	});
+		let nest;
 
-	//set the result to be the last element in the tree
-	this.result = popLast(this.tree);
+		if (str && this.result)
+		{
+			nest = this.result.nest;
 
-	//return true if it reached the desired number of matches
-	return (!this.result && this.matches.length === this.count);
+			//if the last element is a string, then append
+			if (typeof nest[nest.length - 1] === "string")
+			{
+				nest[nest.length - 1] += str;
+				this.result.simple += str;
+			}
+			//otherwise, the new string
+			else
+			{
+				nest.push(str);
+				this.result.simple.push(str);
+			}
+
+			this.result.str += str;
+
+			this.tree.forEach((obj) =>
+			{
+				obj.str += str;
+			});
+		}
+
+		return false;
+	}
+
+	close(): boolean
+	{
+		this.tree.forEach((branch, i) =>
+		{
+			//add the close char to all strings
+			branch.str += this.closeChar;
+		});
+
+		//set the result to be the last element in the tree
+		this.result = popLast(this.tree);
+
+		//return true if it reached the desired number of matches
+		return (!this.result && this.matches.length === this.count);
+	}
 }
 
 export default Extractor
